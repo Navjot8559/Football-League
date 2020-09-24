f25a5238c531d4997bd9e44e0741a3045944c3be
Simran <khairasimran416@gmail.com>
2020-09-24 14:40:08 +0530

    remove managers

---
+++ app/src/main/java/com/navjot/football_league/Model/ItemClickListner.java
@@ -0,0 +1,7 @@
+package com.navjot.football_league.Model;
+
+import android.view.View;
+
+public interface ItemClickListner {
+    void onClick(View view, int position, boolean isLongClick);
+}
--- app/src/main/java/com/navjot/football_league/TeamManagers.java
+++ app/src/main/java/com/navjot/football_league/TeamManagers.java
@@ -1,10 +1,12 @@
 package com.navjot.football_league;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -12,12 +14,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.navjot.football_league.Model.ItemClickListner;
 import com.navjot.football_league.Model.Managers;
 import com.navjot.football_league.Prevelant.Prevelant;
 
@@ -59,10 +66,47 @@ public class TeamManagers extends AppCompatActivity {
 
         FirestoreRecyclerAdapter<Managers,ManagersViewHolder> adapter = new FirestoreRecyclerAdapter<Managers, ManagersViewHolder>(options) {
             @Override
-            protected void onBindViewHolder(@NonNull ManagersViewHolder holder, int position, @NonNull Managers model) {
+            protected void onBindViewHolder(@NonNull ManagersViewHolder holder, int position, @NonNull final Managers model) {
                 holder.managerName.setText(model.getName());
                 holder.managerPhone.setText(model.getPhone());
                 holder.managerTeam.setText(model.getTeamName());
+
+                if(Prevelant.userType.equals("lManager")){
+                    holder.itemView.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            CharSequence options[] = new CharSequence[]{
+                                    "Remove","Cancel"
+                            };
+                            final AlertDialog.Builder builder = new AlertDialog.Builder(TeamManagers.this);
+                            builder.setTitle("Remove Manager");
+                            builder.setItems(options, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                   if(i == 0){
+                                      managerRef.document(model.getPhone()).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                          @Override
+                                          public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Toast.makeText(TeamManagers.this, "Manager Removed", Toast.LENGTH_SHORT).show();
+                                            }
+                                          }
+                                      }).addOnFailureListener(new OnFailureListener() {
+                                          @Override
+                                          public void onFailure(@NonNull Exception e) {
+                                              Toast.makeText(TeamManagers.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                          }
+                                      });
+                                   }
+                                   if(i == 1){
+                                      dialogInterface.cancel();
+                                   }
+                                }
+                            });
+                            builder.show();
+                        }
+                    });
+                }
             }
 
             @NonNull
@@ -78,9 +122,10 @@ public class TeamManagers extends AppCompatActivity {
 
     }
 
-    public static class ManagersViewHolder extends RecyclerView.ViewHolder{
+    public static class ManagersViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         public TextView managerName,managerPhone,managerTeam;
+        private ItemClickListner mItemClickListner;
 
         public ManagersViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -88,6 +133,16 @@ public class TeamManagers extends AppCompatActivity {
             managerPhone = itemView.findViewById(R.id.manager_phone);
             managerTeam = itemView.findViewById(R.id.manager_team);
         }
+
+        @Override
+        public void onClick(View view) {
+            mItemClickListner.onClick(view,getAdapterPosition(),false);
+        }
+
+        public void setItemClickListner(ItemClickListner itemClickListner) {
+            mItemClickListner = itemClickListner;
+        }
+
     }
 
     @Override
